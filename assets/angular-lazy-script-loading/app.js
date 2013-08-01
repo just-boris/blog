d30a21155ee7eb864b12bf28bd19caa2d5b65050
Boris <just-boris@hotmail.com>
2013-08-01 23:40:11 +0400

    демка к статье lazy-load

---
+++ assets/angular-lazy-script-loading/app.js
@@ -0,0 +1,22 @@
+angular.module('blurDemo', ['script'])
+	.directive('blurred', ['$script', function($script) {
+		return function(scope, element, attrs) {
+			//дождемся загрузки плагина и активируем его
+			var onPluginReady = function() {
+				//вызовем конструктор
+				var vague = element.Vague({
+					//TODO можно забирать значение свойства из 
+					//другого атрибута, будет удобно
+		        	intensity : 30
+		    	});
+		    	//а теперь свяжемся с атрибут
+				attrs.$observe('blurred', function(blurred) {
+					vague[blurred === 'true' ? 'blur' : 'unblur']();
+				});
+				scope.$on('$destroy', function() {
+					vague.destroy();
+				});
+			}
+			$script.get('http://gianlucaguarini.github.io/vague.js/Vague.js').then(onPluginReady);
+		}
+	}]);
